fd24c4922ec4423a01c120a509f50748e807e24d
Elladwi <55367319+Elladwi@users.noreply.github.com>
2020-03-17 12:32:37 +0700

    Add files via upload

--- konekDB.php
+++ konekDB.php
@@ -1,15 +1,15 @@
 <?php
 
 // konfigurasi koneksi ke postgre
-$host       =  "10.100.112.80";
-$dbuser     =  "itdev";
-$dbpass     =  "bdk1dev";
-$port       =  "5432";
-$dbname    =  "visionanalytics";
-$dbtable   = "view_sprint_saldoharian"
+$host       =  "";
+$dbuser     =  "";
+$dbpass     =  "";
+$port       =  "";
+$dbname    =  "";
+$dbtable   = ""
 
 // script koneksi php postgree
-$link = new PDO("pgsql:dbname=$dbname;host=$host", $dbuser, $dbpass); 
+$link = new PDO("pgsql:dbname=$dbname;host=$host", $dbuser, $dbpass, $dbtable); 
  
 if($link)
 {
